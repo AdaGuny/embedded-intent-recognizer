233073df36bc0bc47605dacb2e8fc5fb0fd7fa56
SutuSevenKamyoncu <ada.guney.arslan@gmail.com>
2022-04-09 22:09:13 +0200

    Embedded python first lines

--- source/app.cpp
+++ source/app.cpp
@@ -1,5 +1,11 @@
 #include <iostream>
+#include <vector>
+#include <pybind11/pybind11.h>
+#include <pybind11/embed.h>  // python interpreter
+#include <pybind11/stl.h>  // type conversion
 
+namespace py = pybind11;
+using namespace py::literals;
 
 int main() {
     
@@ -7,5 +13,10 @@ int main() {
 
     std::cout << "What do you want to know?" << std::endl;
     std::getline(std::cin, user_input);
+
+    std::cout << "Going embedded..." << std::endl;
+
+    //Start interpreter dies at the end of scope
+    py::scoped_interpreter guard{}; // start the interpreter and keep it alive
 
 }
